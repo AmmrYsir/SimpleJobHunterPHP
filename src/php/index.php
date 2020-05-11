5241fd80d9ff8bf5838cdb33549ef2e0d496ca43
Ammar <ammaryasir03062000@gmail.com>
2020-05-11 21:36:10 +0800

    initial commit v2

---
+++ src/php/index.php
@@ -0,0 +1,13 @@
+<?php 
+    $title = 'SimpleJobHunter | Get that Job!';
+    $style = '<link rel="stylesheet" href="../css/index.css">';
+    
+?>
+
+
+<?php ob_start() ?>
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php include '../template.php'; ?>
