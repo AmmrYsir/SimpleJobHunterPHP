fd857e7bf804f42dfe2ed018d91f596d6782f167
Ammar <ammaryasir03062000@gmail.com>
2020-05-12 08:01:26 +0800

    header final

--- src/php/index.php
+++ src/php/index.php
@@ -8,14 +8,23 @@
 <?php ob_start() ?>
     <header>
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">SimpleJobHunter</a>
+            <a class="navbar-brand" href="#"><em class="fas fa-user-ninja"></em> SimpleJobHunter</a>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Sign In <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
         </nav>
+        <section class="jumbotron jumbotron-fluid bg-dark text-light">
+            <div class="container d-flex justify-content-center align-items-center flex-column">
+                <h1 class="display-3">SimpleJobHunter</h1>
+                <h1 class="display-4">Get the Job right now with one click!</h1>
+            </div>
+        </section>
     </header>
+    <main>
+        
+    </main>
 <?php $content = ob_get_clean(); ?>
 
 <?php include '../template.php'; ?>
